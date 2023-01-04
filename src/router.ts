3dcdf8e9cde5e64b0537f36076eb7af7ae32fab4
Sergey Sudakov <s3gaddtv@gmail.com>
2023-01-04 13:56:58 +0300

    feat: connect user controller into router

--- src/router.ts
+++ src/router.ts
@@ -1,14 +1,16 @@
+import { getUsers, getUser, createUser, updateUser, removeUser } from './controllers/userController';
+
 export const router = async (req, res) => {
     if (req.url === '/api/users' && req.method === 'GET') {
-
+        getUsers(res);
     } else if (req.url === '/api/users' && req.method === 'POST') {
-
+        createUser(req, res);
     } else if (req.url.startsWith('/api/users/') && req.method === 'GET') {
-
+        getUser(req, res);
     } else if (req.url.startsWith('/api/users/') && req.method === 'PUT') {
-
+        updateUser(req, res);
     } else if (req.url.startsWith('/api/users/') && req.method === 'DELETE') {
-
+        removeUser(req, res);
     } else {
         res.writeHead(404, { 'Content-Type': 'application/json' });
         res.end(JSON.stringify({ message: 'Route not found' }));
